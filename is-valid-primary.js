3120c7909e11d90e8e42c9b5cb8894d2d94a5edf
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-02-25 13:21:46 -0600

    [Add all changes]

--- is-valid-primary.js
+++ is-valid-primary.js
@@ -2,6 +2,22 @@
  * YOUR CODE HERE *
  ******************/
 
+function isValidPrimary(primary) {
+  if(primary === 'red') {
+    return true;
+  } else if(primary === 'yellow') {
+    return true;
+  } else if(primary === 'blue') {
+    return true
+  } else if(primary === 'orange') {
+    return false;
+  } else if(primary === 'blorgon') {
+    return false;
+  } else if(primary === 'ted') {
+    return false;
+  }
+} 
+
 
 
 
