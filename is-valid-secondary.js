afce78573ecf8c06f9c9beb1309209a345db753c
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-02-25 13:22:02 -0600

    [Add all changes]

--- is-valid-secondary.js
+++ is-valid-secondary.js
@@ -2,7 +2,21 @@
  * YOUR CODE HERE *
  ******************/
 
-
+function isValidSecondary(primary2) {
+  if(primary2 === 'orange') {
+    return true;
+  } else if( primary2 === 'purple') {
+    return true;
+  } else if(primary2 === 'green') {
+    return true;
+  } else if(primary2 === 'red') {
+    return false;
+  } else if( primary2 === 'black') {
+    return false;
+  } else if( primary2 === 'white') {
+    return false;
+  }
+}
 
 
 // Our setup code here. Don't touch!
